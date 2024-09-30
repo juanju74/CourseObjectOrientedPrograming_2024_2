32031e51deafc7c995ea73969baf26076fac2ce9
juanju74 <juan.arboledamo@campusucc.edu.co>
2024-09-30 14:03:33 +0000

    juan

--- ComposicionAgregacion.java/Banco.java
+++ ComposicionAgregacion.java/Banco.java
@@ -1,14 +1,53 @@
 public class Banco {
-    // Atributos 
-    private Stirng NIT;
-    private String razonSocial;
 
-    // Constructor
-    public Banco(String NIT,String razonSocial)(
-        this.NIT = NIT;
+    //Atributos
+    private String NIT;
+    private String razonSocial;
+    private Cuenta cuenta;
+    
+    public Banco(String nIT, String razonSocial, Cuenta cuenta) {
+        NIT = nIT;
         this.razonSocial = razonSocial;
-    )
-    //metodos 
+        this.cuenta = cuenta;
+    }
+
+    public String getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(String nIT) {
+        NIT = nIT;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Banco [NIT=" + NIT + ", razonSocial=" + razonSocial + ", cuenta=" + cuenta + "]";
+    }
+
+    public boolean esCliente(Cliente cliente){
+        boolean flag = false;
+        if (cliente instanceof Cliente){
+            flag = true;
+        }
+        return flag;
+    }
+    
 
 
 
